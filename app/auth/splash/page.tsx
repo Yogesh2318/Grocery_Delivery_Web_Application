003335a10d5cbe5769a983a5a5e119e155cb59a5
Yogesh vishwanath Bhivsane <yvbhivasane@gmail.com>
2026-05-30 19:02:40 +0530

    routes and zustand added

---
+++ app/auth/splash/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { useEffect } from 'react'
+
+
+
+
+export default function Splash() {
+
+return(
+
+<div className='flex items-center justify-center h-screen'> hello</div>)
+
+}
